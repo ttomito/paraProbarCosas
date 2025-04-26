043123c4da24355248bb2c7f3ae0d6c57de791f9
tomy <craverotomy@gmail.com>
2025-04-26 15:52:55 -0300

    version prueba

---
+++ demostracionRepo.js
@@ -0,0 +1,18 @@
+demostracionRepo.js
+
+console.log(1+1);
+console.log(1+2);
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// Logs all numbers from 0 to 4
+console.log("Hello, world!");
+console.log(1+1);
+console.log(1+2);
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+// Logs all numbers from 0 to 4
+console.log("Hello, world!");
